d629697bc3aeca4bff1249eb5309a4a7b171619f
James Purvis <110248591+JamesPurvis@users.noreply.github.com>
2023-02-17 23:41:11 -0700

    Beginning of login & register

---
+++ src/main/java/me/jamespurvis/blogsite/controller/LoginController.java
@@ -0,0 +1,13 @@
+package me.jamespurvis.blogsite.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login";
+    }
+}
---
+++ src/main/java/me/jamespurvis/blogsite/controller/RegisterController.java
@@ -0,0 +1,13 @@
+package me.jamespurvis.blogsite.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegisterController {
+
+    @GetMapping("/register")
+    public String getRegisterPage() {
+        return "register";
+    }
+}
