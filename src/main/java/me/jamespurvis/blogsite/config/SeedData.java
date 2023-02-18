3a52fcda469400bd2c56b9e643973e66a7d978a3
James Purvis <110248591+JamesPurvis@users.noreply.github.com>
2023-02-17 17:21:47 -0700

    Wrote Seed class for mock data Started Home Controller and mapped the / endpoint Created a simple home html file that shows mock posts from the DB

---
+++ src/main/java/me/jamespurvis/blogsite/config/SeedData.java
@@ -0,0 +1,36 @@
+package me.jamespurvis.blogsite.config;
+
+
+import me.jamespurvis.blogsite.models.Post;
+import me.jamespurvis.blogsite.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class SeedData implements CommandLineRunner {
+
+    @Autowired
+    private PostService postService;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        List<Post> posts = postService.getAll();
+
+        if (posts.size() == 0) {
+            Post post1 = new Post();
+            post1.setTitle("title of post 1");
+            post1.setBody("This is the body of post 1");
+
+            Post post2 = new Post();
+            post2.setTitle(("Title of post 2"));
+            post2.setBody("This is the body of post 2");
+
+            postService.save(post1);
+            postService.save(post2);
+        }
+    }
+}
---
+++ src/main/java/me/jamespurvis/blogsite/controller/HomeController.java
@@ -0,0 +1,26 @@
+package me.jamespurvis.blogsite.controller;
+
+
+import me.jamespurvis.blogsite.models.Post;
+import me.jamespurvis.blogsite.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+
+    @Autowired
+    private PostService postService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<Post> posts = postService.getAll();
+        model.addAttribute("posts", posts);
+        return "home";
+    }
+}
